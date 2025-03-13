2b51d79636f52ceddc757a2f09ddca3bcdf1e496
ajapura <ajapura@bantotal.com>
2025-03-12 22:08:55 -0500

    deploy2

--- app.py
+++ app.py
@@ -112,4 +112,7 @@ def obtener_datos_endpoint():
             return jsonify({'error': 'No se pudo iniciar sesión'}), 401
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    
